8ba78c14f3de00544fa10a4d05db63ada0f42e42
snehal <snehal.webroottech@gmail.com>
2025-09-15 22:34:23 +0530

    dashboard updated

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,12 +1,12 @@
 @extends('user.layouts.master')
 
 @section('breadcrumb')
-    @include('user.components.breadcrumb',['breadcrumbs' => [
-        [
-            'name'  => __("Dashboard"),
-            'url'   => setRoute("user.dashboard"),
-        ]
-    ], 'active' => __("Dashboard")])
+@include('user.components.breadcrumb',['breadcrumbs' => [
+[
+'name' => __("Dashboard"),
+'url' => setRoute("user.dashboard"),
+]
+], 'active' => __("Dashboard")])
 @endsection
 
 @section('content')
@@ -21,7 +21,8 @@
                     <div class="dashbord-item">
                         <div class="dashboard-content">
                             <span class="sub-title">{{__("balance")}}</span>
-                            <h3 class="title">{{ authWalletBalance() }} <span class="text--base">{{ @$baseCurrency->code }}</span></h3>
+                            <h3 class="title">{{ authWalletBalance() }} <span class="text--base">{{ @$baseCurrency->code
+                                    }}</span></h3>
                         </div>
 
                         <div class="dashboard-icon">
@@ -50,23 +51,25 @@
                             <i class="fas fa-paper-plane"></i>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <div class="col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
                     <div class="dashbord-item">
                         <div class="dashboard-content">
                             <span class="sub-title">{{ __("Virtual Card") }}</span>
-                            <h3 class="title">{{ getAmount($data['cardAmount'],2) }} <span class="text--base">{{ @$baseCurrency->code }}</span></h3>
+                            <h3 class="title">{{ getAmount($data['cardAmount'],2) }} <span class="text--base">{{
+                                    @$baseCurrency->code }}</span></h3>
                         </div>
                         <div class="dashboard-icon">
                             <i class="fas fa-credit-card"></i>
                         </div>
                     </div>
-                </div> -->
+                </div>
                 <div class="col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
                     <div class="dashbord-item">
                         <div class="dashboard-content">
                             <span class="sub-title">{{__("Total Bill Pay")}}</span>
-                            <h3 class="title">{{ getAmount($data['billPay'],2) }} <span class="text--base">{{ @$baseCurrency->code }}</span></h3>
+                            <h3 class="title">{{ getAmount($data['billPay'],2) }} <span class="text--base">{{
+                                    @$baseCurrency->code }}</span></h3>
                         </div>
                         <div class="dashboard-icon">
                             <i class="fas fa-shopping-bag"></i>
@@ -77,18 +80,20 @@
                     <div class="dashbord-item">
                         <div class="dashboard-content">
                             <span class="sub-title">{{ __("Total Mobile TopUp") }}</span>
-                            <h3 class="title">{{ getAmount($data['topUps'],2) }} <span class="text--base">{{ @$baseCurrency->code }}</span></h3>
+                            <h3 class="title">{{ getAmount($data['topUps'],2) }} <span class="text--base">{{
+                                    @$baseCurrency->code }}</span></h3>
                         </div>
                         <div class="dashboard-icon">
                             <i class="fas fa-mobile"></i>
                         </div>
                     </div>
                 </div>
-                <!-- <div class="col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <div class="col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
                     <div class="dashbord-item">
                         <div class="dashboard-content">
                             <span class="sub-title">{{ __("Total Withdraw") }}</span>
-                            <h3 class="title">{{ getAmount($data['withdraw'],2) }} <span class="text--base">{{ @$baseCurrency->code }}</span></h3>
+                            <h3 class="title">{{ getAmount($data['withdraw'],2) }} <span class="text--base">{{
+                                    @$baseCurrency->code }}</span></h3>
                         </div>
                         <div class="dashboard-icon">
                             <i class="fas fa-dollar-sign"></i>
@@ -116,11 +121,77 @@
                             <i class="fas fa-gift"></i>
                         </div>
                     </div>
-                </div> -->
+                </div>
             </div>
         </div>
     </div>
-    <!-- <div class="chart-area mt-30">
+    <div class="dashboard-area mt-30">
+        <div class="row">
+            <div class="col-xxl-2 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <a href="{{route('user.transactions.index', 'bill-pay')}}">
+                    <div class="dashbord-item">
+                        <div class="dashboard-content">
+                            <span class="sub-title">Bill Payment</span>
+                            <!-- <h3 class="title"><span class="text--base">USD</span></h3> -->
+                        </div>
+                        <div class="dashboard-icon">
+                            <i class="fas fa-shopping-bag"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xxl-2 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <a href="{{route('user.transactions.index', 'mobile-topup')}}">
+                    <div class="dashbord-item">
+                        <div class="dashboard-content">
+                            <span class="sub-title">Mobile TopUp</span>
+                        </div>
+                        <div class="dashboard-icon">
+                            <i class="fas fa-mobile"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xxl-2 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <a href="{{route('user.transactions.index', 'bill-pay')}}">
+                    <div class="dashbord-item">
+                        <div class="dashboard-content">
+                            <span class="sub-title">Aadhaar(AEPS)</span>
+                        </div>
+                        <div class="dashboard-icon">
+                            <i class="fas fa-mobile"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xxl-2 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <a href="{{route('user.transactions.index', 'bill-pay')}}">
+                <div class="dashbord-item">
+                    <div class="dashboard-content">
+                        <span class="sub-title">mATM(AEPS)</span>
+                    </div>
+                    <div class="dashboard-icon">
+                        <i class="fas fa-mobile"></i>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="col-xxl-2 col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-20">
+                <a href="{{route('user.transactions.index', 'bill-pay')}}">
+                <div class="dashbord-item">
+                    <div class="dashboard-content">
+                        <span class="sub-title">Money Tranfer</span>
+                    </div>
+                    <div class="dashboard-icon">
+                        <i class="fas fa-mobile"></i>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <div class="chart-area mt-30">
         <div class="row mb-20-none">
             <div class="col-xxl-7 col-xl-7 col-lg-7 mb-20">
                 <div class="chart-wrapper">
@@ -128,7 +199,8 @@
                         <h4 class="title">{{ __("Add Money Chart") }}</h4>
                     </div>
                     <div class="chart-container">
-                        <div id="chart1" data-chart_one_data="{{ json_encode($chartData['chart_one_data']) }}" data-month_day="{{ json_encode($chartData['month_day']) }}" class="chart"></div>
+                        <div id="chart1" data-chart_one_data="{{ json_encode($chartData['chart_one_data']) }}"
+                            data-month_day="{{ json_encode($chartData['month_day']) }}" class="chart"></div>
                     </div>
                 </div>
             </div>
@@ -138,13 +210,14 @@
                         <h4 class="title">{{ __("Withdraw Money") }}</h4>
                     </div>
                     <div class="chart-container">
-                        <div id="chart3" data-chart_three_data="{{ json_encode($chartData['chart_two_data']) }}" data-month_day="{{ json_encode($chartData['month_day']) }}" class="chart"></div>
+                        <div id="chart3" data-chart_three_data="{{ json_encode($chartData['chart_two_data']) }}"
+                            data-month_day="{{ json_encode($chartData['month_day']) }}" class="chart"></div>
                     </div>
                 </div>
             </div>
         </div>
-    </div> -->
-    <!-- <div class="dashboard-list-area mt-20">
+    </div>
+    <div class="dashboard-list-area mt-20">
         <div class="dashboard-header-wrapper">
             <h4 class="title">{{ __("Latest Transactions") }}</h4>
             <div class="dashboard-btn-wrapper">
@@ -156,7 +229,7 @@
         <div class="dashboard-list-wrapper">
             @include('user.components.transaction-log',compact("transactions"))
         </div>
-    </div> -->
+    </div>
 </div>
 @endsection
 @push('script')
@@ -167,21 +240,21 @@
     var options = {
         series: [
             {
-            name: "{{ __('Pending') }}",
-            color: "#0C56DB",
-            data: chart_one_data.pending_data
+                name: "{{ __('Pending') }}",
+                color: "#0C56DB",
+                data: chart_one_data.pending_data
             }, {
-            name: "{{ __('Completed') }}",
-            color: "rgba(0, 227, 150, 0.85)",
-            data: chart_one_data.success_data
+                name: "{{ __('Completed') }}",
+                color: "rgba(0, 227, 150, 0.85)",
+                data: chart_one_data.success_data
             }, {
-            name: "{{ __('Canceled') }}",
-            color: "#dc3545",
-            data: chart_one_data.canceled_data
+                name: "{{ __('Canceled') }}",
+                color: "#dc3545",
+                data: chart_one_data.canceled_data
             }, {
-            name: "{{ __('Hold') }}",
-            color: "#ded7e9",
-            data: chart_one_data.hold_data
+                name: "{{ __('Hold') }}",
+                color: "#ded7e9",
+                data: chart_one_data.hold_data
             }
         ],
         chart: {
@@ -199,7 +272,7 @@
         },
         xaxis: {
             type: "datetime",
-            categories:month_day,
+            categories: month_day,
         },
         tooltip: {
             x: {
@@ -210,75 +283,75 @@
 
     var chart = new ApexCharts(document.querySelector("#chart1"), options);
     chart.render();
-//money out
-var chart3 = $("#chart3");
-var chart_three_data = chart3.data("chart_three_data");
-var month_day = chart3.data("month_day");
-// apex-chart
-var options = {
-    series: [
-        {
-            name: "{{ __('Pending') }}",
-            color: "#0C56DB",
-            data: chart_three_data.pending_data
+    //money out
+    var chart3 = $("#chart3");
+    var chart_three_data = chart3.data("chart_three_data");
+    var month_day = chart3.data("month_day");
+    // apex-chart
+    var options = {
+        series: [
+            {
+                name: "{{ __('Pending') }}",
+                color: "#0C56DB",
+                data: chart_three_data.pending_data
             }, {
-            name: "{{ __('Completed') }}",
-            color: "rgba(0, 227, 150, 0.85)",
-            data: chart_three_data.success_data
+                name: "{{ __('Completed') }}",
+                color: "rgba(0, 227, 150, 0.85)",
+                data: chart_three_data.success_data
             }, {
-            name: "{{ __('Canceled') }}",
-            color: "#dc3545",
-            data: chart_three_data.canceled_data
+                name: "{{ __('Canceled') }}",
+                color: "#dc3545",
+                data: chart_three_data.canceled_data
             }, {
-            name: "{{ __('Hold') }}",
-            color: "#ded7e9",
-            data: chart_three_data.hold_data
+                name: "{{ __('Hold') }}",
+                color: "#ded7e9",
+                data: chart_three_data.hold_data
             }
-    ],
-    chart: {
-        type: "bar",
-        height: 350,
-        stacked: true,
-        toolbar: {
-            show: false,
-        },
-        zoom: {
-            enabled: true,
-        },
-    },
-    responsive: [
-        {
-            breakpoint: 480,
-            options: {
-                legend: {
-                    position: "bottom",
-                    offsetX: -10,
-                    offsetY: 0,
-                },
+        ],
+        chart: {
+            type: "bar",
+            height: 350,
+            stacked: true,
+            toolbar: {
+                show: false,
+            },
+            zoom: {
+                enabled: true,
             },
         },
-    ],
-    plotOptions: {
-        bar: {
-            horizontal: false,
-            borderRadius: 10,
+        responsive: [
+            {
+                breakpoint: 480,
+                options: {
+                    legend: {
+                        position: "bottom",
+                        offsetX: -10,
+                        offsetY: 0,
+                    },
+                },
+            },
+        ],
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                borderRadius: 10,
+            },
         },
-    },
-    xaxis: {
-        type: "datetime",
-        categories: month_day,
-    },
-    legend: {
-        position: "bottom",
-        offsetX: 40,
-    },
-    fill: {
-        opacity: 1,
-    },
-};
+        xaxis: {
+            type: "datetime",
+            categories: month_day,
+        },
+        legend: {
+            position: "bottom",
+            offsetX: 40,
+        },
+        fill: {
+            opacity: 1,
+        },
+    };
 
-var chart = new ApexCharts(document.querySelector("#chart3"), options);
-chart.render();
+    var chart = new ApexCharts(document.querySelector("#chart3"), options);
+    chart.render();
 
 </script>
 @endpush
